bbc246ed220fd83bd58a68f2bed4a42568d6863a
SEUNGHYE CHOI <wooaoe68@gmail.com>
2020-02-03 14:49:43 +0900

    20/02/03 spring mvc 수업 02

---
+++ SpringMVC02/src/main/java/com/hello/mvc02/dao/CustomerDao.java
@@ -0,0 +1,13 @@
+package com.hello.mvc02.dao;
+
+import java.util.List;
+
+import com.hello.mvc02.vo.CustomerVo;
+
+public interface CustomerDao {
+	
+	String namespace = "customer";
+	
+	public List<CustomerVo> selectList();
+	
+}
---
+++ SpringMVC02/src/main/java/com/hello/mvc02/dao/CustomerDaoImpl.java
@@ -0,0 +1,21 @@
+package com.hello.mvc02.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.hello.mvc02.vo.CustomerVo;
+
+@Repository
+public class CustomerDaoImpl implements CustomerDao{
+
+	@Override
+	public List<CustomerVo> selectList() {
+		
+		
+		
+		
+		return null;
+	}
+
+}
