e147f56cbc2f26fd4c4c14fc4d655b54814513b0
SEUNGHYE CHOI <wooaoe68@gmail.com>
2020-02-06 21:04:30 +0900

    20/02/06 spring mvc filter

---
+++ SpringMVC3_filter/src/main/java/com/mvc/upgrade/model/biz/BoardBizImpl.java
@@ -0,0 +1,46 @@
+package com.mvc.upgrade.model.biz;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mvc.upgrade.model.dao.BoardDao;
+import com.mvc.upgrade.model.vo.BoardVo;
+
+@Service
+public class BoardBizImpl implements BoardBiz{
+
+	@Autowired
+	private BoardDao dao;
+	
+	
+	@Override
+	public List<BoardVo> selectList() {
+		
+		return dao.selectList();
+	}
+
+	@Override
+	public BoardVo selectOne(int myno) {
+		
+		return dao.selectOne(myno);
+	}
+
+	@Override
+	public int insert(BoardVo vo) {
+		
+		return dao.insert(vo);
+	}
+
+	@Override
+	public int update(BoardVo vo) {
+		return dao.update(vo);
+	}
+
+	@Override
+	public int delete(int myno) {
+		return dao.delete(myno);
+	}
+
+}
